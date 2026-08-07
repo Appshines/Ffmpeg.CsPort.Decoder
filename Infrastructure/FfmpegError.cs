14861bcacd2e6440acb71a77f55255dbe9a9f800
Dan <Appshines@users.noreply.github.com>
2026-08-07 14:37:36 +0200

    Add Infrastructure sources
    Publish the FFmpeg-derived managed C# source files for Infrastructure.

---
+++ Infrastructure/FfmpegError.cs
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: The respective FFmpeg copyright holders; see UPSTREAM-COPYRIGHTS.txt
+// SPDX-FileCopyrightText: 2026 Ffmpeg.CsPort.Decoder contributors
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ * This file is part of Ffmpeg.CsPort.Decoder, an independent C# port of FFmpeg.
+ *
+ * This file belongs to a C# translation and modified work of FFmpeg source code from commit
+ * 9b6c8969e05b4f0b29f0f85cd501be6b3e582e6b:
+ * https://github.com/FFmpeg/FFmpeg/tree/9b6c8969e05b4f0b29f0f85cd501be6b3e582e6b
+ * The exact upstream-file mapping and the original copyright notices are recorded in
+ * PORTED-FROM-FFMPEG.md and UPSTREAM-COPYRIGHTS.txt.
+ *
+ * Created or modified 2026-08-06: translated from C to C# or added as managed port support.
+ *
+ * This library is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this library. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * PORT-NOTE: 1:1 translation. Do not refactor, reorder, or simplify; bit-exactness
+ * against the FFmpeg reference is verified by the conformance tests.
+ */
+namespace Ffmpeg.CsPort.Decoder.Infrastructure
+{
+	/// <summary>
+	/// Preserves FFmpeg return-code values so internal parser control flow does not depend on exceptions.
+	/// </summary>
+	public static class FfmpegError
+	{
+		public const int InvalidArgument = -22;
+		public const int TryAgain = -11;
+		public const int OutOfMemory = -12;
+		public const int NoSpace = -28;
+		public const int NotImplemented = -38;
+		public const int Range = -34;
+		public const int InvalidData = -1094995529;
+		public const int EndOfFile = -541478725;
+		public const int PatchWelcome = -1163346256;
+	}
+}
